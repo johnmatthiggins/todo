b8315ae402443be5a11f38788957f300a16035c4
John Higgins <johnmatthiggins@gmail.com>
2025-03-22 09:24:26 -0700

    added login and logout views to endpoints file

--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth import aget_user
-from django.http import JsonResponse
+from django.contrib.auth import aget_user, alogin, alogout, aauthenticate
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponseBadRequest, JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.views.generic import View
 
 # Create your views here.
 def home(request):
@@ -16,3 +17,40 @@ async def current_user(request):
         "name": user,
         "email": user.email
     }], safe=False)
+
+class Login(View):
+    async def get(self, request: HttpRequest) -> HttpResponse:
+        user = await aget_user(request)
+        if user.is_authenticated:
+            return HttpResponseRedirect("/")
+        return render(
+            template_name="home.html",
+            request=request,
+            context={},
+        )
+
+    async def post(self, request):
+        if not 'username' in request.POST or not 'password' in request.POST:
+            return HttpResponseBadRequest()
+
+        user = await aauthenticate(
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        if user is None:
+            user = await aget_user(request)
+            return render(
+                template_name="home.html",
+                request=request,
+                context={},
+            )
+        else:
+            await alogin(request, user)
+            return HttpResponseRedirect('/')
+
+class Logout(View):
+    async def post(self, request):
+        user = await aget_user(request)
+        if user.is_authenticated:
+            await alogout(request=request)
+        return HttpResponseRedirect('/')
